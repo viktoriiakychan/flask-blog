d2963fe8ff24a737082a9b2fcee4ec88912f0491
viktoriiakychan <viktoriia.kychan@gmail.com>
2023-07-15 16:19:48 +0100

    almost done

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml, os
+from flask_login import LoginManager, login_required, logout_user 
+
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 Bootstrap(app)
 
 db = yaml.safe_load(open("settings.yaml"))
@@ -14,6 +18,8 @@ app.config["MYSQL_DB"] = db["mysql_db"]
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 app.config["SECRET_KEY"] = os.urandom(24)
 mysql = MySQL(app)
+
+@login_manager.user_loader
 
 @app.route("/")
 def index():
@@ -37,11 +43,66 @@ def posts(id):
 
 @app.route("/register/", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        user_details = request.form
+        cursor = mysql.connection.cursor()
+        
+        
+        user_by_username = cursor.execute("SELECT * FROM users WHERE username = %s", ([user_details["username"]]))
+        user_by_email = cursor.execute("SELECT * FROM users WHERE email = %s", ([user_details["email"]]))
+       
+        if not user_details["first_name"]: flash("Enter first name!", "danger")
+        elif not user_details["last_name"]: flash("Enter last name!", "danger")
+        elif not user_details["username"]: flash("Enter username!", "danger")
+        elif not user_details["email"]: flash("Enter email!", "danger")
+        elif not user_details["password"]: flash("Enter password!", "danger")
+        elif not user_details["confirm_password"]: flash("Enter password confirmation!", "danger")
+        elif user_by_username > 0:
+            flash("This username is already taken", "danger")
+            return redirect("/register")
+        elif user_by_email > 0:
+            flash("This email is already taken", "danger")
+            return redirect("/register")    
+        elif user_details["password"] != user_details["confirm_password"]:
+            flash("Passwords do not match!", "danger")
+            return redirect("/register")
+        else:
+            cursor.execute("INSERT INTO users(id, first_name, last_name, username, email, password) VALUES (%s, %s, %s, %s, %s, %s)",("", user_details['first_name'], user_details['last_name'], user_details['username'], user_details['email'], generate_password_hash(user_details['password'])))
+            cursor.connection.commit()
+            cursor.close()
+            flash("You were successefully registered", "success")
+            return redirect("/login")
+    
     return render_template("register.html")
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        user_details = request.form
+        username = user_details["username"]
+        cursor = mysql.connection.cursor()
+        result = cursor.execute("SELECT * FROM users WHERE username = %s", ([username]))
+        if result > 0:
+            user = cursor.fetchone()
+            if check_password_hash(user['password'], user_details['password']):
+                session['login'] = True
+                session['first_name'] = user['first_name']
+                session['last_name'] = user['last_name']
+                flash("Welcome, " + session['first_name'] + "!")
+            else: 
+                cursor.close()
+                flash("Username or password is incorrect!", "danger")
+                return redirect("/login")
+        cursor.close()
+        session['username'] = username
+        return redirect('/')
     return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    session.clear()
+    flash("You have been logged out!", "info")
+    return redirect("/")
 
 @app.route("/new-post/", methods=["GET", "POST"])
 def new_post():
@@ -67,9 +128,7 @@ def my_posts():
         return render_template("my_posts.html", posts=posts)
     return render_template("my_posts.html", posts=None)
 
-@app.route("/logout/")
-def logout():
-    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
